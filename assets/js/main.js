8e7a78ea857e8b702f039262c460b8d0b4b19f3e
= <=>
2023-04-30 21:18:27 +0300

    new update in home and footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,6 @@
-/**
-* Template Name: Moderna
-* Updated: Mar 10 2023 with Bootstrap v5.2.3
-* Template URL: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
+
+
 (function() {
   "use strict";
 
